f65cd427a5dad779c1c5ba4756bd91f66c9e64c5
Lucas Dário <lucasdario.dariolucas@gmail.com>
2017-10-18 17:15:59 -0200

    Add permanent gravity and implement is_on_floor function

--- game/Objects/Player.gd
+++ game/Objects/Player.gd
@@ -8,7 +8,7 @@ const JUMP_SPEED = -2000
 
 var acc = ACC
 var velocity = Vector2()
-var jumping = false
+var jumping = 0  # 0 = parado. 1 = 1o pulo. 2 = 2o pulo.
 
 func _ready():
 	set_fixed_process(true)
@@ -16,25 +16,23 @@ func _ready():
 func _fixed_process(delta):
 	motion_change(delta)
 	
-	var motion = velocity * delta
-	motion = move(motion)
-	
-	if (is_colliding()):
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
 
 func is_on_floor():
-	#CADÊ?
-	return true
+	if(is_colliding()):
+		var collider = get_collider()
+		if (collider.get_type() == "TileMap"):
+			var n = get_collision_normal()
+			if (n.x == 0 and n.y == -1):
+				return true
+	return false
 
 func motion_change(time_stamp):
 	#velocity.y = VERTICAL_VEL
+	velocity.y = velocity.y + GRAVITY
 
 	var left = Input.is_action_pressed("ui_left")
 	var right = Input.is_action_pressed("ui_right")
-	var jump = Input.is_action_pressed("ui_jump")
+	var jump_action = Input.is_action_pressed("ui_jump")
 	
 	if(left):
 		velocity.x = -HORIZONTAL_VEL
@@ -43,18 +41,20 @@ func motion_change(time_stamp):
 	else:
 		velocity.x = 0
 	
-	if(jump):
-		if(!jumping and is_on_floor()):
-			jumping = true
+	if(jump_action):
+		if(jumping != 2):
+			jumping += 1
 			velocity.y = JUMP_SPEED
 
-	if(jumping):
-		velocity.y = velocity.y + GRAVITY
+	if(jumping > 0):
 		#Se colidir com algum objeto ou plataforma, ele para de "pular"
-		if(velocity.y == -JUMP_SPEED):
-			jumping = false
+		if(is_on_floor()):
+			jumping = 0
 			velocity.y = 0
 	
-	
 	var motion = velocity * time_stamp
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
 	move(motion)
