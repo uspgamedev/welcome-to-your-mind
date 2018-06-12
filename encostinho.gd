35b3fccbc0c8b25c9b147af3dce19245f110bba6
Victor Domiciano <victor.dm3@gmail.com>
2018-06-12 17:50:26 -0300

    Add encostinho routine

--- encostinho.gd
+++ encostinho.gd
@@ -1,7 +1,21 @@
 extends KinematicBody
 
-func _on_Area_body_entered(body):
-    if body.is_in_group('player'):
-        body.encostinhos.append(self)
-        body.encostinho_colision()
+const NORM_GRAV = -24.8
+const MAX_SLOPE_ANGLE = 40
 
+var vel = Vector3(5, 0, 0)
+
+func _on_Area_body_entered(body):
+	if body.is_in_group('player'):
+		body.encostinhos.append(self)
+		body.encostinho_colision()
+
+func _physics_process(delta):
+	
+	var grav = NORM_GRAV
+	vel.y += delta*grav
+	
+	move_and_slide(vel)
+
+func _on_Timer_timeout():
+	vel.x *= -1
