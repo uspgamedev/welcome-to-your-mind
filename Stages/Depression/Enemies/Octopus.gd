80e9ddab3aea1e856405c751f71f3656771b627d
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-10-02 13:14:41 -0300

    Added Octopus animations and rotation

--- Stages/Depression/Enemies/Octopus.gd
+++ Stages/Depression/Enemies/Octopus.gd
@@ -6,9 +6,12 @@ export (PackedScene)var TriggerWave = null
 const SPEED_MULTIPLIER = 1
 const ATK_OFFSET = 40
 const ATK_SPEED = 700
+const ANGLE_MULTIPLIER = 1.6
 
 onready var Target = get_parent().get_node("Player")
 onready var initialy = get_global_position().y
+
+var angle = 0
 
 func _physics_process(delta):
 	follow(Target, delta)
@@ -21,19 +24,21 @@ func follow(Target, delta):
 	var speed = abs(dist.x) * SPEED_MULTIPLIER
 	var velocity = direction * speed * delta
 	set_position(get_position() + velocity)
+	angle = (get_global_position() + Vector2(0, ATK_OFFSET)).angle_to_point(Target.get_global_position())
+	$Sprite.set_rotation_degrees(cos(angle) * 10 * ANGLE_MULTIPLIER)
 
 func start_attacking(Target):
 	$AttackDuration.start()
+	$AnimationPlayer.play("Attacking")
 	attack(Target)
 
 func attack(Target):
 	$AttackInterval.start()
 	
-	var dir = (get_global_position() + Vector2(0, ATK_OFFSET)).angle_to_point(Target.get_global_position())
-	var angle = Vector2(-cos(dir), -sin(dir))
+	var vec_angle = Vector2(-cos(angle) * ANGLE_MULTIPLIER, -sin(angle))
 	var pos = get_position() + Vector2(0, ATK_OFFSET)
 	var TrigWv = TriggerWave.instance()
-	TrigWv.DIRECTION = angle
+	TrigWv.DIRECTION = vec_angle
 	TrigWv.SPEED = ATK_SPEED
 	TrigWv.set_position(pos)
 	get_parent().add_child(TrigWv)
@@ -47,3 +52,4 @@ func _on_AttackInterval_timeout():
 
 func _on_AttackDuration_timeout():
 	$AttackCooldown.start()
+	$AnimationPlayer.play("Charging")
