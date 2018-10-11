454ba508de9ab2fa2b8aab931369eeccd3f25758
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-10-11 14:33:23 -0300

    Added LeftDoor

--- Stages/Depression/PuzzleOctopus.gd
+++ Stages/Depression/PuzzleOctopus.gd
@@ -3,3 +3,11 @@ extends Node2D
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		$Octopus.activate(body)
+		lower_left_door()
+
+func lower_left_door():
+	var LD = $LeftDoor
+	var Twn = $LeftDoor/Tween
+	
+	Twn.interpolate_property(LD, "position", LD.get_position(),  LD.get_position() + Vector2(0, 170), 1.0, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	Twn.start()
