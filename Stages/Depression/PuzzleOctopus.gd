10bd08f749b8c2383d3708ccd1652f83727344e5
Detril <andremoukarzel@hotmail.com>
2018-10-23 17:40:18 -0300

    Add JumpIntro and tweaks to a lot of stuff

--- Stages/Depression/PuzzleOctopus.gd
+++ Stages/Depression/PuzzleOctopus.gd
@@ -6,8 +6,19 @@ func _on_Area2D_body_entered(body):
 		lower_left_door()
 
 func lower_left_door():
-	var LD = $LeftDoor
-	var Twn = $LeftDoor/Tween
+	var LD = $Wall/LeftDoor
+	var Twn = $Wall/LeftDoor/Tween
 	
 	Twn.interpolate_property(LD, "position", LD.get_position(),  LD.get_position() + Vector2(0, 170), 1.0, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	Twn.start()
+
+func lower_right_door():
+	var RD = $Wall/RightDoor
+	var Twn = $Wall/RightDoor/Tween
+	
+	Twn.interpolate_property(RD, "position", RD.get_position(),  RD.get_position() + Vector2(0, 170), 1.0, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	Twn.start()
+
+func _on_ExitArea_body_entered(body):
+	if body.is_in_group("Player"):
+		lower_right_door()
