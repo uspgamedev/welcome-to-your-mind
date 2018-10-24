a486a9d2fc9823f0068a728497e3651f60d768c6
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-10-23 22:21:49 -0300

    TriggerSpawn tweaks
    - Automatized TriggerSpawn signal connection

--- Stages/Depression/Enemies/TriggerGood.gd
+++ Stages/Depression/Enemies/TriggerGood.gd
@@ -1,12 +1,13 @@
 extends "res://Stages/Depression/Enemies/TriggerBase.gd"
 
-signal trigger_die
+signal trigger_absorbed(Absorber)
 
 func reached_target(target):
 	set_physics_process(false)
-	if(target.is_in_group("Player")):
-		target.can_jump = true	
-		emit_signal("trigger_die")
+	
+	if target.is_in_group("Player"):
+		target.can_jump = true
+		emit_signal("trigger_absorbed", target)
 		die()
 	else:
 		set_physics_process(true)
--- Stages/Depression/Enemies/TriggerSpawn.gd
+++ Stages/Depression/Enemies/TriggerSpawn.gd
@@ -1,21 +1,28 @@
 extends Node2D
 
+var Trigger_scn = load("res://Stages/Depression/Enemies/TriggerGood.tscn")
+var Player
 var hasTrigger = false
 
 func _ready():
 	spawn()
-	hasTrigger = true
 
 func spawn():
-	var trigger = load("res://Stages/Depression/Enemies/TriggerGood.tscn")
-	var trigger_instance = trigger.instance()
-	trigger_instance.connect("trigger_die", self, "trigger_died")
-	add_child(trigger_instance)
-	
-func trigger_died():
+	instance_Trigger()
+	hasTrigger = true
+
+func instance_Trigger():
+	var Trig = Trigger_scn.instance()
+	Trig.connect("trigger_absorbed", self, "trigger_gone")
+	add_child(Trig)
+
+func trigger_gone(Absorber):
+	Player = Absorber
+	if not Player.is_connected("jumped", self, "Player_jumped"):
+		Player.connect("jumped", self, "Player_jumped")
 	hasTrigger = false
 
-func _on_Player_jumped():
+func Player_jumped():
 	if hasTrigger == false:
 		$Timer.start()
 		hasTrigger = true
