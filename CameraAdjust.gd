86393069848ae2efcd805ba75f323f04c69c77b7
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2018-10-31 11:39:36 -0300

    CameraAdjust fix
    Since we now only change the camera's limits, removing the camera from player is not only unneeded, but incorrect.

--- CameraAdjust.gd
+++ CameraAdjust.gd
@@ -3,16 +3,16 @@ extends Node2D
 # A CollisionShape2D for Area2D must be added to each instance of this node after it is added to a scene.
 # Camera values will be updated and transitioned when Player, with a child Camera2D node, enters said Area2D.
 
-export (int) var left_limit = -10000
-export (float, 3.0) var left_time = 0.5
-export (int) var right_limit = 10000
-export (float, 3.0) var right_time = 0.5
-export (int) var top_limit = -10000
-export (float, 3.0) var top_time = 0.5
-export (int) var bottom_limit = 10000
-export (float, 3.0) var bottom_time = 0.5
-export (float, 0.3, 2) var zoom = 1.0
-export (float, 3.0) var zoom_time = 0.5
+export (int) var left_limit = -100000000
+export (float, 5.0) var left_time = 0.5
+export (int) var top_limit = -100000000
+export (float, 5.0) var top_time = 0.5
+export (int) var right_limit = 100000000
+export (float, 5.0) var right_time = 0.5
+export (int) var bottom_limit = 100000000
+export (float, 5.0) var bottom_time = 0.5
+export (float, 0.3, 3) var zoom = 1.0
+export (float, 5.0) var zoom_time = 0.5
 
 
 func _on_Area2D_body_entered(body):
@@ -23,8 +23,7 @@ func adjust_Camera(Cam):
 	var p_pos = get_parent().get_position()
 	var Player = Cam.get_parent()
 	
-	take_Camera_from_Player(Player, Cam)
-	
+#	take_Camera_from_Player(Player, Cam)
 	$Tween.interpolate_property(Cam, "limit_left", Cam.limit_left,  left_limit + p_pos.x, left_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property(Cam, "limit_right", Cam.limit_right,  right_limit + p_pos.x, right_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property(Cam, "limit_top", Cam.limit_top,  top_limit + p_pos.y, top_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
@@ -32,8 +31,7 @@ func adjust_Camera(Cam):
 	$Tween.interpolate_property(Cam, "zoom", Cam.zoom,  Vector2(zoom, zoom), zoom_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.start()
 	yield($Tween, "tween_completed")
-	
-	give_Camera_to_Player(Player, Cam)
+#	give_Camera_to_Player(Player, Cam)
 
 func take_Camera_from_Player(Player, Cam):
 	Player.remove_child(Cam)
